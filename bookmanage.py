57679d0f2ae2153257a16abeb01af3124033fe5d
syarin03 <gami5507@gmail.com>
2022-05-30 23:50:13 +0900

    연습해본거

--- bookmanage.py
+++ bookmanage.py
@@ -19,12 +19,33 @@ class Book():
 
 
 con = sqlite3.connect("book.db", isolation_level=None)
-
 c = con.cursor()
 
-c.execute("SELECT * FROM Books where num = '122968'")
+"""
+c.execute("SELECT * FROM booktbl where bookid = '122968'")
+print(c.fetchall()[0])
 
-print(c.fetchall()[0][0])
+c.execute("SELECT * FROM booktbl where pubplace = '인천'")
+print(len(c.fetchall()))
+
+c.execute("SELECT * FROM booktbl where bookname like '%베르%'")
+print(len(c.fetchall()))
+
+c.execute("SELECT * FROM booktbl where bookname like '%베르%'")
+for i in c.fetchall():
+    print(i[5] + " <> " + i[6])
+    
+c.execute("SELECT * FROM booktbl where bookname like '%"+temp+"%'")
+for i in c.fetchall():
+    print(i[5] + " <> " + i[6])
+"""
+
+temp = input()
+
+
+c.execute("SELECT bookname, writer, lib FROM booktbl where bookname like '%"+temp+"%' and isrent = 0")
+for i in c.fetchall():
+    print(i[0] + " <> " + i[1] + " <> " + i[2])
 
 """
 c.execute("UPDATE Books SET publisher = '하랑' where num = '122968'")
